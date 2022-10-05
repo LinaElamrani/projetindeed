b41cb36d8252e7cc610e3b31210e70891f8d42bc
Lina <Linelamrani@gmail.com>
2022-10-06 01:36:21 +0200

    task04

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -7,7 +8,19 @@ def get_db_connection():
     return conn
 
 
+def get_job(job_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM jobs WHERE job_id = ?',
+                        (job_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
+
 app= Flask(__name__)
+app.config['SECRET_KEY'] = 'projetindeed'
 
 @app.route("/")
 def index():
@@ -16,7 +29,71 @@ def index():
     conn.close()
     return render_template('index.html', jobs=jobs)
 
+@app.route('/<int:job_id>')
+def job(job_id):
+    job = get_job(job_id)
+    return render_template('job.html', job=job)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        location = request.form['location']
+        contract = request.form['contract']
+        worktime = request.form['worktime']
+        postalcode = request.form['postalcode']
+        userstate = request.form['userstate']
+        salary = request.form['salary']
 
 
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO jobs (title, description, location, postalcode, contract, worktime, userstate, salary ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                         (title, description, location, postalcode, contract, worktime, userstate, salary))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    job= get_job(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        location = request.form['location']
+        contract = request.form['contract']
+        worktime = request.form['worktime']
+        postalcode = request.form['postalcode']
+        userstate = request.form['userstate']
+        salary = request.form['salary']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE jobs SET title = ?, description = ?, location = ?, postalcode = ?, contract = ?, worktime = ?, userstate = ?, salary = ?'
+                         ' WHERE job_id = ?',
+                         (title, description, location, postalcode, contract, worktime, userstate, salary, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', job=job)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    job = get_job(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM jobs WHERE job_id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(job['title']))
+    return redirect(url_for('index'))
 
 # Methode .fetchall() permet de récupérer d'un coup l'ensemble du résultat d'une requête
