8ce3bffac10fb42d7b585667ff6eb24e2cf3fbe3
Lina <Linelamrani@gmail.com>
2022-10-05 14:03:49 +0200

    step2

--- init_db.py
+++ init_db.py
@@ -9,12 +9,16 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO jobs (title, location, postalcode, contract, worktime, userstate,description, salary, category_id, company_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            ('title jobs', 'location for the first jobs' ,'postalcode for the first job','contract for the first job','worktime for the first job','userstate for the first job','description for the first job','salary for the first job','category_id for the first job','company_id for the first job')
+            ('Developer', 'location for the first job' ,'postalcode for the first job','contract for the first job','worktime for the first job','userstate for the first job','description for the first job','salary for the first job','category_id for the first job','company_id for the first job')
             )
 
-# cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-#             ('Second Post', 'Content for the second post')
-#             )
+cur.execute("INSERT INTO jobs (title, location, postalcode, contract, worktime, userstate,description, salary, category_id, company_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            ('Data analyst', 'location for the job2' ,'postalcode for the job2','contract for the job2','worktime for the job2','userstate for the job2','description for the job2','salary for the job2','category_id for the job2','company_id for the job2')
+            )
+
+cur.execute("INSERT INTO jobs (title, location, postalcode, contract, worktime, userstate,description, salary, category_id, company_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            ('Community Manager', 'location for the job3' ,'postalcode for the job3','contract for the job3','worktime for the job3','userstate for the job3','description for the job3','salary for the job3','category_id for the job3','company_id for the job3')
+            )
 
 connection.commit()
 connection.close()
---
+++ server.py
@@ -0,0 +1,22 @@
+import sqlite3
+from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+app= Flask(__name__)
+
+@app.route("/")
+def index():
+    conn = get_db_connection()
+    jobs = conn.execute('SELECT * FROM jobs').fetchall()
+    conn.close()
+    return render_template('index.html', jobs=jobs)
+
+
+
+
+# Methode .fetchall() permet de récupérer d'un coup l'ensemble du résultat d'une requête
